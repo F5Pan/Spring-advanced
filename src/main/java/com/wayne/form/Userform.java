5657d5b5de2368bc61f724c2e3d2c4cd2311363d
F5Pan <storyj14@gmail.com>
2020-05-03 23:05:49 +0800

    程式新增: 1.前端回傳的東西 比原先的model還要多欄位 所以建立一個UserForm來承接 但只需要回傳user物件回資料庫做資料的CRUD 重複的getter、setter 會讓程式過於的冗長 所以使用BeanUtils.copyProperties(userform, user); 來複製userform的資料到user 2.建立interface--FormConvert  定義一個方法來讓其他class實現 3.在userform的bean上面添加註解(詳見README) 並檢查輸入密碼是否一致 添加註解@Valid指定前端回傳的物件是要經過驗證的且通過BindingResult 取得驗證後的結果 4.可以將自己程式邏輯的判斷加到BindingResult來顯示 使用rejectValue來添加字段

---
+++ src/main/java/com/wayne/form/Userform.java
@@ -0,0 +1,92 @@
+package com.wayne.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.BeanUtils;
+
+import com.wayne.domain.User;
+
+public class Userform {
+
+	// public static final String PHONE_REG =
+	// "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";
+	public static final String PHONE_REG = "^09[0-9]{8}$";
+
+	@NotBlank
+	private String username;
+	@NotBlank
+	@Length(min = 6, message = "密碼最少需要6位")
+	private String password;
+	@Pattern(regexp = PHONE_REG, message = "請輸入正確的手機號碼")
+	private String phone;
+	@Email
+	private String email;
+	@NotBlank
+	private String confirmPassword;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public boolean confirmPassword() {
+		if (this.password.equals(this.confirmPassword)) {
+			return true;
+		}
+		return false;
+	}
+
+	public User convertToUser() {
+		User user = new UserFormConvert().convert(Userform.this);
+		return user;
+	}
+
+	private class UserFormConvert implements FormConvert<Userform, User> {
+
+		@Override
+		public User convert(Userform userform) {
+			User user = new User();
+			BeanUtils.copyProperties(userform, user);
+			return user;
+		}
+	}
+
+}
--- src/main/java/com/wayne/web/LoginController.java
+++ src/main/java/com/wayne/web/LoginController.java
@@ -1,13 +1,19 @@
 package com.wayne.web;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wayne.domain.User;
 import com.wayne.domain.UserRepository;
+import com.wayne.form.Userform;
 
 @Controller
 public class LoginController {
@@ -26,13 +32,24 @@ public class LoginController {
 	}
 
 	@PostMapping("/register")
-	public String registerPost(@RequestParam String username, @RequestParam String password, @RequestParam String email,
-			@RequestParam Integer phone) {
-		User user = new User();
-		user.setUsername(username);
-		user.setPassword(password);
-		user.setEmail(email);
-		user.setPhone(phone);
+	public String registerPost(@Valid Userform userform, BindingResult result) {
+		  boolean boo = true;
+	        if (!userform.confirmPassword()) {
+	            result.rejectValue("confirmPassword","confirmError","密碼不一致");
+	            boo = false;
+	        }
+	        if (result.hasErrors()) {
+	            List<FieldError> fieldErrors = result.getFieldErrors();
+	            for (FieldError error : fieldErrors) {
+	                System.out.println(error.getField() + " : " + error.getDefaultMessage() + " : " + error.getCode());
+	            }
+	            boo = false;
+	        }
+
+	        if (!boo) {
+	            return "register";
+	        }
+		User user = userform.convertToUser();
 		userRepository.save(user);
 		return "redirect:/login";
 	}
